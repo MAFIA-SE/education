e107c49363fe89263ba655bd59dc45c144e83f94
MAFIA <azibekovacynara@gmail.com>
2026-08-24 21:55:20 +0600

    I did tasks

--- src/Conditional_Operators/ConditionalOperatorsTask.java
+++ src/Conditional_Operators/ConditionalOperatorsTask.java
@@ -5,13 +5,34 @@ public class ConditionalOperatorsTask {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 //Task 1
-        System.out.println("Enter any number, you can enter before and after 0: ");
+      /*  System.out.println("Enter any number, you can enter before and after 0: ");
         int num = scanner.nextInt();
     if (num >= 0){
         System.out.println("Positive number: " + num);
     }else{
         System.out.println("Negative number: " + num);
-    }
+    }*/
+//Task 2
+        /*System.out.println("Enter first number: ");
+        int a = scanner.nextInt();
+        System.out.println("Enter second number: ");
+        int b = scanner.nextInt();
+        System.out.println("Enter third number: ");
+        int c = scanner.nextInt();
 
-  }
+        if (a >= b && a >= c){
+            System.out.println("The greatest number: " + a);
+        } else if (b >= a && b >= c){
+            System.out.println("The greatest number: " + b);
+        }else {
+            System.out.println("The greatest number: " + c);
+        }*/
+//Task 3
+        System.out.println("Enter the number of points: ");
+        int points = scanner.nextInt();
+
+        System.out.println((points >=50)? "You passed": "You didn't pass");
+
+
+    }
 }
